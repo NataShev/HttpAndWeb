efd19a68ac4b1428dce2420f63c08f3ae01ec4fe
Natalya Shevchenko <nsevcenko1988@gmail.com>
2022-09-07 23:35:35 +0300

    Changes in Server.java

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Server {
+    final int FIX_CONST = 64;
 
     private int port;
 
@@ -16,7 +17,7 @@ public class Server {
         try (final var serverSocket = new ServerSocket(this.port)) {
             while (true) {
                 try (final var socket = serverSocket.accept()) {
-                    ExecutorService es = Executors.newFixedThreadPool(64);
+                    ExecutorService es = Executors.newFixedThreadPool(FIX_CONST);
                     final Future<?> submit = es.submit(new RunServer(socket));
                 }
             }
